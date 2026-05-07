94b34427eef594620a6f7b080ff534bcb7ee4fab
ninhatzo <anac.estudantes2@gmail.com>
2026-05-06 21:50:15 -0300

    Adicionando gráficos

--- View/src/js/script.js
+++ View/src/js/script.js
@@ -38,3 +38,70 @@ const dropdownToggle = document.getElementById("navbarDropdown");
   
   const currentActiveTab = document.querySelector('.tab-btn.active');
   tabClicked(currentActiveTab);
+
+// ===== GRÁFICO MÊS =====
+const labelsMes = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago','Set','Out','Nov','Dez'];
+
+const dataMes = {
+  labels: labelsMes,
+  datasets: [{
+    label: 'Cadastro por mês (2026)',
+    data: [65, 59, 80, 81],
+    fill: false,
+    borderColor: '#1ba8ff',
+    tension: 0.1
+  }]
+};
+
+const configMes = {
+  type: 'line',
+  data: dataMes,
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        labels: {
+          boxWidth: 0
+        }
+      }
+    }
+  }
+};
+
+const ctxMes = document.getElementById('meuGraficoMes').getContext('2d');
+new Chart(ctxMes, configMes);
+
+
+// ===== GRÁFICO ANO =====
+const labelsAno = ['2020', '2021', '2022', '2023','2024','2025','2026'];
+
+const dataAno = {
+  labels: labelsAno,
+  datasets: [{
+    label: 'Cadastro por ano',
+    data: [680, 550, 500, 550, 570, 500, 600],
+    fill: false,
+    borderColor: '#026402',
+    tension: 0.1
+  }]
+};
+
+const configAno = {
+  type: 'line',
+  data: dataAno,
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        labels: {
+          boxWidth: 0
+        }
+      }
+    }
+  }
+};
+
+const ctxAno = document.getElementById('meuGraficoAno').getContext('2d');
+new Chart(ctxAno, configAno);
